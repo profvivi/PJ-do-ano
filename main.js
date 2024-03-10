65c39abd4af14f86d89b61b061b00249047e3fcf
Viviane <106716101+profvivi@users.noreply.github.com>
2024-03-10 17:34:05 -0300

    s

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*A declaração const indica que se trata de uma variável que não será alterada
+ em nenhuma instância do código.Isso significa que serão sempre quatro botões.
+ Vamos declarar a variável const botoes, e atribuir a ela um comando que busca tudo
+ que existe a classe botão.Queremos todas as informações relacionadas à classe, 
+ por isso utilizaremos o querySelectorAll(), cujo interior dos parênteses receberá um
+ .botao entre aspas, assim como nomeamos uma classe.*/
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
