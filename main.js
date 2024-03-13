9378b55b8e88ad9bf9204990ce79893dc1cdf58e
Viviane <106716101+profvivi@users.noreply.github.com>
2024-03-13 11:48:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ botoes[i].classList.add("ativo").*/
  laço de repetição for() dentro das chaves da function(), acima da linha que adiciona a classe.
  Esse laço receberá entre parênteses a variável let j=0, em seguida, adicionaremos o j++.
  Em vez de adicionar, vamos remover. Portanto, adicionaremos entre as chaves do for() um botoes na posição j 
- junto ao .classList.remove("ativo").*/ 
+ junto ao .classList.remove("ativo").
 
 const botoes = document.querySelectorAll(".botao");
 
@@ -40,4 +40,25 @@ for(let i=0;i <botoes.length;i++){
 
         botoes[i].classList.add("ativo");
     }
+
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
