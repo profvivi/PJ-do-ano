c57083f811463cc132ea770eecbe6faa2837e07d
Viviane <106716101+profvivi@users.noreply.github.com>
2024-03-17 23:33:42 -0300

    s

--- main.js
+++ main.js
@@ -65,4 +65,4 @@ for (let i = 0; i < botoes.length; i++) {
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;*/
