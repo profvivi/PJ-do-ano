37a180cd3427d328b7f6a9ffd4bc1bb3c69ce7e8
Viviane <106716101+profvivi@users.noreply.github.com>
2024-03-31 23:42:29 -0300

    salvar

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ for(let i=0;i <botoes.length;i++){
 }
 */
 
-//Aula 5
+/*Aula 5
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
@@ -67,6 +67,28 @@ let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
+*/
 
+//Aula 6
 
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
