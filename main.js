75666c240891ba526e7c8a4e8cf445dbc212834d
Viviane <106716101+profvivi@users.noreply.github.com>
2024-03-31 23:40:30 -0300

    salvar

--- main.js
+++ main.js
@@ -67,19 +67,6 @@ let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
-//aula 6
 
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
 
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
+
