09fa0d2130813ac411ec884957ca08ae96cd11cd
Viviane <106716101+profvivi@users.noreply.github.com>
2024-04-02 23:11:59 -0300

    s

--- main.js
+++ main.js
@@ -127,7 +127,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-04-05T00:00:00");
 
 
 //chamar uma função chamada calculaTempo(), que terá como parâmetro de entrada o tempoObjetivo1.
