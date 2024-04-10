df9c31ce946fd67d9dc37cd5c7c340af55892351
Viviane <106716101+profvivi@users.noreply.github.com>
2024-04-10 11:38:51 -0300

    s

--- main.js
+++ main.js
@@ -275,7 +275,8 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
         return [dias,horas,minutos,segundos];
     } else {
-        return [0,0,0,0];
+        //return [0,0,0,0];
+        return "Prazo Finalizado";
     }
 }
 
