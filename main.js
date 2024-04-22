ad7b371e565154743b1ee0d486a174898a51b834
Viviane <106716101+profvivi@users.noreply.github.com>
2024-04-21 23:28:23 -0300

    s

--- main.js
+++ main.js
@@ -109,7 +109,6 @@ function calculaTempo(tempoObjetivo) {
 }*/
 
 //Aula7 - Automatizando atualizações
-/*
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
@@ -124,6 +123,27 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
 
 /*1ª explicação
@@ -236,7 +256,7 @@ que seja exibida uma mensagem de que o prazo foi esgotado. Dessa
 forma, a contagem do tempo que resta ocorrerá apenas se esse tempo
 for positivo.*/
 
-const botoes = document.querySelectorAll(".botao");
+/*const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
@@ -294,4 +314,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-comecaCronometro();
+comecaCronometro();*/
